e8f074c3110ab36be4d80b9307e52404fcbde670
kotesh-arya <koteswarraomudila@gmail.com>
2024-09-10 01:10:41 +0530

    FEAT: Add client side routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,34 @@ import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 import Dashboard from "./components/Dashboard";
 import LinePlot from "./components/LinePlot";
-
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    errorElement: (
+      <div>
+        404 Not Found <Link className="text-blue-500">Back to main page</Link>
+      </div>
+    ),
+  },
+  {
+    path: "/signin",
+    element: <Signin />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="">
+      <RouterProvider router={router} />
       {/* <Signin /> */}
-      <Dashboard />
+      {/* <Dashboard /> */}
       {/* <LinePlot  /> */}
       {/* <Signup /> */}
     </div>
